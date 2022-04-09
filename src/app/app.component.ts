2e19efee6fc234cc5c719a820bf12798fcb2ad34
KeitaHidaka <keitinn.reader@gmail.com>
2022-04-09 18:07:15 +0900

    編集画面でタスクを追加する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  tasks = [
-    {
-      name: 'ライブの申込み',
-      isCompleted: false,
-    },
-    {
-      name: 'セトリの予想',
-      isCompleted: true,
-    },
-  ];
-
-  addTask(taskName: string) {
-    this.tasks.push({
-      name: taskName,
-      isCompleted: false,
-    });
-  }
-
-  // 完了しているタスク数を返す
-  getNumOfCompletedTasks() {
-    let cnt = 0;
-    for (let task of this.tasks) {
-      if (task.isCompleted) {
-        cnt++;
-      }
-    }
-    return cnt;
-  }
-}
+export class AppComponent {}
---
+++ src/app/task-manager.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { TodoTask } from './todo-task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskManagerService {
+  tasks: TodoTask[];
+  constructor() {
+    this.tasks = [];
+    this.loadTasks();
+  }
+
+  addTask(taskName: string) {
+    this.tasks.push({
+      name: taskName,
+      isCompleted: false,
+    });
+    this.saveTasks();
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  loadTasks() {
+    let jsonString = window.localStorage.getItem('tasks');
+    if (jsonString) {
+      this.tasks = JSON.parse(jsonString);
+    }
+  }
+
+  saveTasks() {
+    let jsonString = JSON.stringify(this.tasks);
+    window.localStorage.setItem('tasks', jsonString);
+  }
+
+  // 完了しているタスク数を返す
+  getNumOfCompletedTasks() {
+    let cnt = 0;
+    for (let task of this.tasks) {
+      if (task.isCompleted) {
+        cnt++;
+      }
+    }
+    return cnt;
+  }
+}
